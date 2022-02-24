389e5d8b77e05ba2031e9d9c01efe0a38a595279
EnderGolem <>
2022-02-24 13:07:21 +0300

    add Zanyt2

---
+++ Zanyt2/Zanyt2/Zanyt2.cpp
@@ -0,0 +1,42 @@
+﻿// Zanyt2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <list>
+#include "MyList.h"
+
+using namespace std;
+
+int main()
+{
+	{
+		MyList<int> lst{ 1,6,2,-3,4,-5,-4 , 6 };
+		cout << "Count_ positive: ";
+		cout << count_positive(lst) << '\n';
+		//int count,mx;
+		auto [mx, count] = count_maxim(lst);
+		cout << "Maxim and count: ";
+		cout << mx << ' ' << count << '\n';
+		MyList<int> n{};
+		MyList<int> p{};
+		split_the_list_into_two(lst, p);
+		lst.print_from_head();
+		p.print_from_head();
+
+	}
+	{
+		MyList<int> lst{ 1,2,3,4,5 };
+		reverse(lst);
+		lst.print_from_head();
+	}
+	{
+		MyList<int> list1, list2;
+		list1.push_back(10);
+		cout << (++--list1.end() == list1.end()) << endl;
+		cout << (list2.end() != list1.end()) << endl;
+		cout << (list1.end() == list1.end()) << endl;
+		cout << *(--list1.end()) << endl;
+	}
+
+}
