a1d6f0753fc7d242ccaf86ed8bb4686e1b825a1b
EnderGolem <83020228+EnderGolem@users.noreply.github.com>
2022-03-02 17:35:18 +0300

    add insert

--- Zanyt3/Zanyt3/BinaryTree.h
+++ Zanyt3/Zanyt3/BinaryTree.h
@@ -1,12 +1,68 @@
 #pragma once
 
 template<class  T>
-class  binary_tree
+class  BinaryTree
 {
 private:
-	struct  node
+	struct  Node
 	{
 		T data;
-		node* parent, * left, * right;
+		Node* parent, * left, * right;
 	};
+public:
+	Node* m_root;
+	BinaryTree(): m_root(nullptr)
+	{
+	}
+	void insert(T data)
+	{
+		if(m_root->data  == nullptr)
+		{
+			m_root = Node{ data,nullptr,nullptr,nullptr };
+			return;
+		}
+		auto current = m_root;
+		while(current != nullptr)
+		{
+			if (current->data < data) {
+				if (current->left == nullptr) {
+					current->left = Node{ data,current,nullptr,nullptr };
+					return;
+				}
+				current = current->left;
+			}			
+			else
+			{
+				if (current->right == nullptr) {
+					current->right = Node{ data,current,nullptr,nullptr };
+					return;
+				}
+				current = current->right;
+			}
+		}
+	}
+	bool contains(T data)
+	{
+		if (m_root->data == nullptr)
+		{
+			m_root = Node{ data,nullptr,nullptr,nullptr };
+			return  false;
+		}
+		auto current = m_root;
+		while (current != nullptr)
+		{
+			if (current->data == data)
+				return true;
+			if (current->data < data) 
+			{			
+				current = current->left;
+			}
+			else
+			{
+				current = current->right;
+			}
+		}
+		return false;
+	}
+	
 };
--- Zanyt3/Zanyt3/Zanyt3.cpp
+++ Zanyt3/Zanyt3/Zanyt3.cpp
@@ -1,8 +1,9 @@
 ﻿#include <iostream>
+#include "BinaryTree.h"
 
 using namespace  std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
 }
