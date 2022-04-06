bcd4a8f3320e0627988b808680ab322f94ff093e
EnderGolem <83020228+EnderGolem@users.noreply.github.com>
2022-04-06 21:10:51 +0300

    Add realization of unordered map
    Добавлена моя реализация через эту стурктуру и не моя.

--- Zanyt 6/Zanyt 6/main.cpp
+++ Zanyt 6/Zanyt 6/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <unordered_map>
 #include "tree_for_words.h"
 using namespace std;
 
@@ -87,7 +88,7 @@ void realize_tree_for_words_char()
 			c = in.get();
 			if ('A' <= c && c <= 'Z') // ѕо приколу сравни потом без такого сравнени€
 				c += 32;
-			tree.plus_word(c);
+			tree.plus_word(c);		
 		}
 	}
 	else
@@ -103,8 +104,82 @@ void realize_tree_for_words_char()
 	cout << chrono::duration_cast<chrono::milliseconds>(t3 - t2).count() <<" "<< chrono::duration_cast<chrono::milliseconds>(t3 - t1).count() << '\n';
 }
 
+void realize_unordered_map()
+{
+	unordered_map<string, int> mp;
+	chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
+	std::ifstream in("War and Peace.txt");
+	if (in.is_open())
+	{
+		string s;
+		while (in>>s)
+		{
+			if (s.back() < 'a' || s.back() > 'z')
+			{
+				s.pop_back();
+				while (s.length() > 0 && (s.back() < 'a' || s.back() > 'z'))
+				{
+					s.pop_back();
+				}
+
+			}
+			if(s.length() > 0 && 'A' <= s[0] && s[0] <= 'Z')
+			{
+				s[0] += 32;
+			}
+			if(!s.empty())
+				mp[s]++;
+		}
+		
+	}
+	else
+	{
+		cout << "Couldn't open file\n";
+	}
+	in.close();
+	chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
+	cout << chrono::duration_cast<chrono::milliseconds>(t2 - t1).count() << '\n';
+}
+
+void find_replace(string &str, const string & substr)
+{
+	size_t f;
+	while((f = str.find(substr)) != string::npos)
+	{
+		str.replace(f, substr.length(), "");
+	}
+}
+
+void remove_delimers(string s)
+{
+	find_replace(s, ",");
+	find_replace(s, ".");
+	find_replace(s, "!");
+	find_replace(s, "?");
+	find_replace(s, " ");
+}
+void to_lower(std::string& str)
+{
+	transform(str.begin(), str.end(),str.begin() ,std::tolower);
+}
+void realize_unordered_map_not()
+{
+	unordered_map<string, int> mp;
+	chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
+	std::ifstream in("War and Peace.txt");
+	string s;
+	for(in>>s;!in.eof();in>>s)
+	{
+		to_lower(s);
+		remove_delimers(s);
+		++mp[s];
+	}
+	in.close();
+	chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
+	cout << chrono::duration_cast<chrono::milliseconds>(t2 - t1).count() << '\n';
+}
+
 int main()
 {
-	//realize_set();
-	realize_tree_for_words_char();
+	realize_unordered_map_not();
 }
