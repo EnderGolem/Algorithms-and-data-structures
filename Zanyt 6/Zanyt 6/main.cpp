a96deb35dbc4143fb9ea39f0ca87c706cfb66b14
EnderGolem <83020228+EnderGolem@users.noreply.github.com>
2022-04-06 18:02:53 +0300

    New function
    Вынес функцию из main
    Создал дополнительные функции, чтобы можно было сравнить скорость.

--- Zanyt 6/Zanyt 6/main.cpp
+++ Zanyt 6/Zanyt 6/main.cpp
@@ -1,12 +1,46 @@
 #include <chrono>
 #include <iostream>
 #include <fstream>
+#include <map>
 #include "tree_for_words.h"
 using namespace std;
 
+/// <summary>
+/// Реализация через set
+/// </summary>
+void realize_set()
+{
+	map<string, int> mp;
+	chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
+	std::ifstream in("War and Peace.txt");
+	if (in.is_open())
+	{
+		
+		string s;
+		while(in>>s)
+		{
+			mp[s]++;
+		}
+	
+		/*
+		char c;
+		while (in.peek() != EOF)
+		{
+			c = in.get();
+			tree.plus_word(c);
+		}
+		*/
+	}
+	else
+	{
+		cout << "Couldn't open file\n";
+	}
+	in.close();
+	chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
+	cout << std::chrono::duration_cast<chrono::milliseconds>(t2 - t1).count() << '\n';
+}
 
-
-int main()
+void realize_tree_for_words_string()
 {
 	cout << int('\r') << int('\n') << '\n';
 	tree_for_words tree{};
@@ -14,13 +48,40 @@ int main()
 	std::ifstream in("War and Peace.txt");
 	if(in.is_open())
 	{
-		/*
+		
 		string s;
 		while(in>>s)
 		{
-			tree_for_words(s);
+			tree.plus_word(s);
 		}
-		*/
+		
+		/*
+		char c;
+		while(in.peek()  != EOF)
+		{
+			c = in.get();
+			tree.plus_word(c);
+		}*/
+	}
+	else
+	{
+		cout << "Couldn't open file\n";
+	}
+	in.close();
+	chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
+	cout << chrono::duration_cast<chrono::milliseconds>(t2 - t1).count() << '\n';
+}
+
+void realize_tree_for_words_char()
+{
+	cout << int('\r') << int('\n') << '\n';
+	tree_for_words tree{};
+	chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
+	std::ifstream in("War and Peace.txt");
+	if(in.is_open())
+	{
+		
+		
 		char c;
 		while(in.peek()  != EOF)
 		{
@@ -34,6 +95,11 @@ int main()
 	}
 	in.close();
 	chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
-	//tree.print_words();
-	cout << std::chrono::duration_cast<chrono::milliseconds>(t2 - t1).count() << '\n';
+	cout << chrono::duration_cast<chrono::milliseconds>(t2 - t1).count() << '\n';
+}
+
+int main()
+{
+	realize_set();
+	//realize_tree_for_words_string();
 }
