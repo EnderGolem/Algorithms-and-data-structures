dfe0d875b26a58acb4666b57599f2ac768ca978a
EnderGolem <83020228+EnderGolem@users.noreply.github.com>
2022-04-07 00:30:07 +0300

    Fix split

--- Zanyt 6/Zanyt 6/main.cpp
+++ Zanyt 6/Zanyt 6/main.cpp
@@ -259,22 +259,52 @@ void realize_unordered_map_third()
 
 //4 - 6
 
-void print( string*& const arr, int n = 16)
+void print(string*& const arr, int n = 16)
 {
 	for (int i = 0; i < n; i++)
 	{
-		cout << arr[i] << ' ';
+		cout << arr[i] << ',';
 	}
 	cout << '\n';
 }
 
-void split(string s, string* &arr)
+void split(string s, string*& arr)
 {
-	int n = 16;
-	arr = new string[n];
-	auto first = s.begin();
+	int n = 1;
 	auto g = s.begin();
-	for (int i = 0; i < n; i++)
+	while (true)
+	{
+
+		g = (find(g, s.end(), ','));
+		if (g == s.end())
+			break;
+		++g;
+		n++;
+	}
+	arr = new string[16];
+	auto first = s.begin();
+	g = s.begin();
+	string basic_string = "";
+	if (n > 16)
+	{
+		for (int i = 0; i < n - 16; i++)
+		{
+			g = (find(g, s.end(), ','));
+			basic_string += s.substr(first - s.begin(), g - first);
+			++g;
+			first = g;
+		}
+	}
+	else
+	{
+
+		g = (find(g, s.end(), ','));
+		basic_string = s.substr(first - s.begin(), g - first);
+		++g;
+		first = g;
+	}
+	arr[0] = basic_string;
+	for (int i = 1; i < 16; i++)
 	{
 		g = (find(g, s.end(), ','));
 		arr[i] = s.substr(first - s.begin(), g - first);
@@ -282,10 +312,6 @@ void split(string s, string* &arr)
 		first = g;
 	}
 }
-
-//Name Platform Year_of_Release Genre Publisher NA_Sales EU_Sales JP_Sales
-//Other_Sales Global_Sales Critic_Score Critic_Count User_Score
-//User_Count Developer Rating;
 //Задача 4. На основе приведённой базы компьютерных игр для каждого издателя указать суммарный доход по различным жанрам игр.
 void fourth()
 {
@@ -298,7 +324,6 @@ void fourth()
 		string* arr;
 		split(s, arr);
 		mp[arr[4]][arr[3]] += atof(arr[9].c_str());
-	//	cout<<arr[0]<<" " << arr[9] << '\n';
 
 		delete[] arr;
 	}
@@ -306,19 +331,59 @@ void fourth()
 	{
 		double sum = 0;
 		cout << "Publisher: " << x.first << '\n';
-		for(auto y : x.second)
+		for (auto y : x.second)
 		{
-			//cout<<"     " << y.first << " " << y.second << '\n';
 			sum += y.second;
 		}
-		cout<<"     "  << "Sum: " << sum << '\n';
+		cout << "     " << "Sum: " << sum << '\n';
 	}
 	ifs.close();
-	
+
 }
+
+//Name, Platform, Year_of_Release, Genre, Publisher, NA_Sales, EU_Sales, JP_Sales, Other_Sales,
+//Global_Sales, Critic_Score, Critic_Count, User_Score, User_Count, Developer, Rating
+
+//Задача 5. Вычислить наиболее доходный жанр игровой индустрии по каждому году. 
+void fifth()
+{
+	ifstream ifs("Video_Games.csv");
+	string s;
+	unordered_map<string, map<string, double>> mp;
+	getline(ifs, s);
+	while (getline(ifs, s))
+	{
+		string* arr;
+		split(s, arr);
+		mp[arr[2]][arr[3]] += atof(arr[9].c_str());
+		delete[] arr;
+	}
+	for (auto x : mp)
+	{
+		string ans = "S";
+		cout << "Year: " << x.first << '\n';
+		for (auto y : x.second)
+		{
+			if (y.second > mp[x.first][ans])
+			{
+				ans = y.first;
+			}
+		}
+		cout << "     " << "Max income: " << ans << '\n';
+	}
+	ifs.close();
+
+}
+
+//Men of War : Assault Squad, PC, 2011, Strategy, 1C Company, 0.01, 0.03, 0, 0.01, 0.05, 77, 21, 8.2, 93, "THQ, 1C, 1C Company", T
+
 //Wii Sports,Wii,2006,Sports,Nintendo
 //41.36 28.96 3.77 8.45 82.53,76,51,8,322,Nintendo,E
 int main()
 {
-	fourth();
+
+	/*string* arr;
+	split("Triple Pack: Xbox Live Arcade Compilation,X360,2011,Misc,Microsoft Game Studios,0.03,0,0,0,0.03,,,tbd,,Various,T", arr);
+	print(arr, 16);*/
+	fifth();
 }
