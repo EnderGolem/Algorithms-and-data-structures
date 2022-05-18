218fd5aea20836ce4cc723952113ca20dd464978
EnderGolem <83020228+EnderGolem@users.noreply.github.com>
2022-05-19 00:41:59 +0300

    Update Dejkstra

--- Zanyt 10/Zanyt 10/main.cpp
+++ Zanyt 10/Zanyt 10/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 
 
@@ -7,48 +8,90 @@ using namespace  std;
 int n, m;
 int* dist;
 bool* used;
-int** a;
+int* trace;
+int** mas;
 
-void Dejkstra(const int &s = 0)
+void Dejkstra(const int& s = 0)
 {
+	
+	priority_queue<int> q;
 	dist[s] = 0;
-	for (int i = 0; i < n; i++)
+	q.push(s);
+	while (!q.empty()) {
+		{
+			int current = q.top();
+			q.pop();
+			//что-то
+			used[current] = true;
+
+			for (int y = 0; y < m; y++)
+			{
+				if (mas[current][y] >= 0)
+					if (dist[current] + mas[current][y] < dist[y])
+					{
+						dist[y] = dist[current] + mas[current][y];
+						trace[y] = current;
+						q.push(y);
+					}
+			}
+		}
+	}
+}
+
+void print_test()
+{
+	if (trace[n - 1] == -1)
 	{
-		int current = 0;
-		for (int j = 0; j < n; j++)
-		{
-			if (!used[j] && dist[current] > dist[j])
-				j = current;
-		}
-		if(dist[current] == INT32_MAX)
-			break;
-		used[current] = true;
-		
-		for (int y = 0; y < m; y++)
-		{
-			if(a[current][y] >= 0)
-				dist[y] = min(dist[y], dist[current] + a[current][y]);
-		}
+		cout << -1 << '\n';
+		return;
+	}
+	vector<int> vec;
+	vec.push_back(n - 1);
+	while (vec.back() != -1)
+	{
+		vec.push_back(trace[vec.back()]);
+	}
+	vec.pop_back();
+	reverse(vec.begin(), vec.end());
+
+	for (auto value : vec)
+	{
+		cout << value + 1 << ' ';
 	}
 }
 
 int main()
 {
+	cin >> n >> m;
 	dist = new int[n];
 	used = new bool[n];
-	for (int i = 0; i < n; i++) {
+	trace = new int[n];
+	for (int i = 0; i < n; i++) 
+	{
 		dist[i] = INT32_MAX;
 		used[i] = false;
+		trace[i] = -1;
 	}
-	a = new int* [m];
-	for (int i = 0; i < m; i++)
+	mas = new int* [n];
+	for (int i = 0; i < n; i++)
 	{
-		a[i] = new int[m];
-		for (int j = 0; j < m; j++)
+		mas[i] = new int[n];
+		for (int j = 0; j < n; j++)
 		{
-			a[i][j] = -1;
+			mas[i][j] = -1;
 		}
 	}
-	
 
+
+	for(int i = 0;i<m;i++)
+	{
+		int a, b,cost;
+		cin >> a >> b>>cost;
+		a--; b--;
+		mas[a][b] = cost;
+		mas[b][a] = cost;		
+	}
+	Dejkstra(0);
+
+	print_test();
 }
