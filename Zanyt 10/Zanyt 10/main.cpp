f686bee4657bcd18415c5862c8a852db4c49707f
EnderGolem <83020228+EnderGolem@users.noreply.github.com>
2022-05-19 01:59:32 +0300

    final Dejkstra

--- Zanyt 10/Zanyt 10/main.cpp
+++ Zanyt 10/Zanyt 10/main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+п»ї#include <iostream>
 #include <queue>
 #include <vector>
+#include <algorithm>
 
 
 using namespace  std;
@@ -9,34 +10,40 @@ int n, m;
 int* dist;
 bool* used;
 int* trace;
-int** mas;
+short** mas;
 
 void Dejkstra(const int& s = 0)
 {
 	
-	priority_queue<int> q;
+	priority_queue<pair<int, int>> q;
+	
 	dist[s] = 0;
-	q.push(s);
-	while (!q.empty()) {
+	q.push({ 0,s });
+	
+	while (!q.empty())
+	{
+		int current = q.top().second;
+		if(q.top().first > dist[current])
 		{
-			int current = q.top();
 			q.pop();
-			//что-то
-			used[current] = true;
-
-			for (int y = 0; y < m; y++)
-			{
-				if (mas[current][y] >= 0)
-					if (dist[current] + mas[current][y] < dist[y])
-					{
-						dist[y] = dist[current] + mas[current][y];
-						trace[y] = current;
-						q.push(y);
-					}
-			}
+			continue;
 		}
+		q.pop();
+		used[current] = true;
+		for (int y = 0; y < n; y++)
+		{			
+			if (mas[current][y] >= 0)
+				if (dist[current] + mas[current][y] < dist[y])
+				{
+					dist[y] = dist[current] + mas[current][y];
+					trace[y] = current;
+					q.push({ dist[y] ,y });
+				}
+		}		
+
 	}
 }
+
 
 void print_test()
 {
@@ -66,16 +73,16 @@ int main()
 	dist = new int[n];
 	used = new bool[n];
 	trace = new int[n];
-	for (int i = 0; i < n; i++) 
+	for (int i = 0; i < n; i++)
 	{
 		dist[i] = INT32_MAX;
 		used[i] = false;
 		trace[i] = -1;
 	}
-	mas = new int* [n];
+	mas = new short* [n];
 	for (int i = 0; i < n; i++)
 	{
-		mas[i] = new int[n];
+		mas[i] = new short[n];
 		for (int j = 0; j < n; j++)
 		{
 			mas[i][j] = -1;
@@ -83,15 +90,19 @@ int main()
 	}
 
 
-	for(int i = 0;i<m;i++)
+	for (int i = 0; i < m; i++)
 	{
-		int a, b,cost;
-		cin >> a >> b>>cost;
+		int a, b, cost;
+		cin >> a >> b >> cost;
 		a--; b--;
 		mas[a][b] = cost;
-		mas[b][a] = cost;		
+		mas[b][a] = cost;
 	}
 	Dejkstra(0);
-
 	print_test();
+
+
+	delete[] dist;
+	delete[] used;
+	delete[] trace;
 }
